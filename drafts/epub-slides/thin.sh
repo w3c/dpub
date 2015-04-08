8d4516ee8ac5e05429db293f5d9aa8c58ac3776f
Ivan Herman <ivan@ivan-herman.net>
2015-04-08 12:14:22 +0200

    Removed the traces of sliderizer; added a better video handling to the draft presentation

--- drafts/epub-slides/thin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# killall thin
-
-thin -c _OUTPUT/ -p 3333 -a 192.168.1.68 -A file start &
-
-open http://192.168.1.68:3333/content/EPUB3/epub/nav.xhtml
-
-#open http://localhost:3000/content/index.html
